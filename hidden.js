afe44924e4ffb481255cbbdb3f44831c00418ef7
fatima8988 <fatima.chohan@elev.ga.lbs.se>
2026-02-10 15:43:09 +0100

    46

--- hidden.js
+++ hidden.js
@@ -13,7 +13,7 @@ import {
 
 const ALLOWED_EMAILS = [
   "mi423ma@gmail.com",
-  "niclaskuzio844@gmail.com"
+  "Niclaskuzio844@gmail.com"
 ].map(e => e.toLowerCase());
 
 const loginBtn = document.getElementById("loginBtn");
